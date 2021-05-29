fd21f0ed0cb37031a86d68ad99ecede5d14a7941
Ruth Silva <83378357+Ruthsilvapereira@users.noreply.github.com>
2021-05-29 13:14:16 -0400

    feat: LinkedList.h by Ruth

--- LinkedList.h
+++ LinkedList.h
@@ -1,6 +1,7 @@
 //Olá a todos! Estou adiantando a atividade Lista Simplesmente Ligada, conforme video aula disponivel em:https://drive.google.com/file/d/1U7DY59DzPNdC5vpBSg_brgJ_WAo-NQHU/view
 //LinkedList.h ======> by Ruth (postada completa com as operaçoes, conforme proposto no video:https://drive.google.com/file/d/1U7DY59DzPNdC5vpBSg_brgJ_WAo-NQHU/view)
-//Lembrando que a lista a ser criada em grupo é a LinkedList.c ===> onde todos vão editar e escrever parte do codigo juntos
+//LinkedList.h, exemplo disponivel em:http://www.jppreti.com/2019/07/15/lista-simplesmente-ligada/#LinkedListh
+//Lembrando que a lista a ser criada em grupo é a ===>LinkedList.c <=== já criada e iniciada por by Ruth, na LinkedList.c todos vão editar e escrever parte do codigo juntos no mesmo projeto
 #ifndef DataStructure_LinkedList_h
 #define DataStructure_LinkedList_h
 enum boolean {
