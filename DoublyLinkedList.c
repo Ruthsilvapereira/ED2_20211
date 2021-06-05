695ab3ac1aea0df219a9e07f2f38cebaf45d58af
Ruth Silva <ruthsilva1994@gmail.com>
2021-06-05 18:01:59 +0000

    feat: DoublyLinkedList.c by Ruth

---
+++ DoublyLinkedList.c
@@ -0,0 +1,41 @@
+//Olá a todos! Estou adiantando a atividade Lista Duplamente Ligada, atividade dois do AVA
+//O QUE É?
+//Veremos que o uso da Lista Duplamente Ligada é indicada para situações que precise voltar, ou seja, navegar entre as posições.
+//Caso a necessidade seja apenas de avançar e nunca voltar, então o indicado é a Lista Simplesmente Ligada vista anteriormente.
+//Dito isso, vejamos o funcionamento das funções usadas na Lista Simplesmente Ligada, usando a Lista Duplamente Ligada. Nota-se que as funções são um pouco semelhantes a anterior, porém, as funções são colocadas de forma a corresponder a Lista Duplamente Ligada (bidirecional).
+//Biblioteca da Lista Duplamente Ligada (DoublyLinkedList.h)
+//Operações complementares: 
+//          2.2.1 show 
+//          2.2.2 showMem
+//      Descrição da atividade
+//   	DoublyLinkedList.c
+//   	DoublyLinkedList.h
+//   	DoublyLinkedListTest.c 
+//   	Por gentileza coloque o nome a frente da sua escolha de implementação, comente sempre seu nome em tudo que foi feito e detalhe para melhor endendimento dos demais.
+//		  Divisão da atividade por alunos, conforme Lista Simplesmente Ligada (primeira lista). Porém, agora será para Lista Duplamente Ligada (caso queira mudar a função de escolha é só editar).
+//   		3.2.1 init ======> Ruth
+//   		3.2.2 isEmpty ======> Leandro
+//   		3.2.3 enqueue ======> Matheus Santiago
+//   		3.2.4 first ======>Gabriel Robert
+//   		3.2.5 last ======>Jose Guilherme Neves
+//   		3.2.6 dequeue ======> Carlos Henrique Teixeira Carneiro
+//   		3.2.7 pop ======> Zhaira Rachid Jaudy
+//   		3.2.8 top ======> Thiago Ramalho Setúbal
+//   		3.2.9 push ======> Vinicius Matusita
+//   		3.2.10 getNodeByPos ======>Lucio Lisboa
+//   		3.2.11 getPos ======> Alessandra Mirelle
+//   		3.2.12 add ======> Guilherme Mendes 
+//   		3.2.13 addAll ======> Hans Maciel
+//   		3.2.14 removePos ======>Wallatan França
+//   		3.2.15 indexOf ======> by Eduardo Hideki and Wenderson Farias
+//   		3.2.16 removeData ======> Mickael Luiz Dias da Silva
+//          2.2.1 show ======>
+//          2.2.2 showMem ======>
+// início
+#include <stdio.h>
+#include <stdlib.h>
+#include "DoublyLinkedList.h"
+//Feat: init by Ruth
+void init(DoublyLinkedList *list) {
+	//RUTH
+}
